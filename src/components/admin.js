bb59fa182b2e85588e4388ee42d6e2db2b5e2763
IkpemosiArnold <61964760+IkpemosiArnold@users.noreply.github.com>
2025-02-03 11:26:15 +0100

    edited import area

--- src/components/admin.js
+++ src/components/admin.js
@@ -5,7 +5,7 @@ import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Addteam from './addteam';
-import PhotoUpload from "./photoupload"; 
+import PhotoUpload from './PhotoUpload'; 
 
 export default function Admin() {
 
