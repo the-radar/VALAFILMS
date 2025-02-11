659d382e60fbf106449b83cbdfe8841904dcca6b
IkpemosiArnold <61964760+IkpemosiArnold@users.noreply.github.com>
2025-02-11 06:04:12 +0100

    removed unsafe values

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2m504Ph_Jyf8-JupX9fOHz90biK5Z1CU",
-  authDomain: "valafilms-5d3a6.firebaseapp.com",
-  databaseURL: "https://valafilms-5d3a6-default-rtdb.firebaseio.com",
-  projectId: "valafilms-5d3a6",
-  storageBucket: "valafilms-5d3a6.appspot.com",
-  messagingSenderId: "338857177426",
-  appId: "1:338857177426:web:31018719eb4dae76bf0a8b",
-  measurementId: "G-50N0313RW6"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+console.log(firebaseConfig);
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export const signInWithEmailAndPassword = async (email, password) => {
